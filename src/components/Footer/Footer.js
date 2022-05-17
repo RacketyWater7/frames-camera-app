f39f14187e49ed5950cdec45388389416d8d533a
RacketyWater7 <haseebudeen@outlook.com>
2022-05-17 12:41:40 +0500

    added frames and properly set their dimension

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,11 @@
-import styles from './Footer.module.scss';
+import styles from "./Footer.module.scss";
 
 const Footer = ({ ...rest }) => {
   return (
     <footer className={styles.footer} {...rest}>
-      <p>
-        &copy; <a href="https://spacejelly.dev">Space Jelly</a>, {new Date().getFullYear()}
-      </p>
+      <p></p>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
